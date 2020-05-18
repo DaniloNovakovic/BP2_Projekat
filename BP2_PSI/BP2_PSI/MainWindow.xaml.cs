96f1dd2be2aebe1911dea9bfa3d21833e5585558
Danilo Novakovic <dakenzi97@gmail.com>
2020-05-18 17:10:32 +0200

    Created Unit of Work factory and ability to select active configuration string in the App.config

--- BP2_PSI/BP2_PSI/MainWindow.xaml.cs
+++ BP2_PSI/BP2_PSI/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Core.Interfaces;
+using Persistance;
+using System;
+using System.Configuration;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BP2_PSI
 {
@@ -20,9 +11,22 @@ namespace BP2_PSI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly IUnitOfWork _uow;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            var nameOrConnectionString = ConfigurationManager.AppSettings.Get("SelectedConnectionStringName");
+            var factory = new UoWFactory(nameOrConnectionString);
+            _uow = factory.CreateNew();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            _uow.Dispose();
+
+            base.OnClosed(e);
         }
     }
 }
---
+++ BP2_PSI/Persistance/UoWFactory.cs
@@ -0,0 +1,24 @@
+﻿using Core.Interfaces;
+
+namespace Persistance
+{
+    public class UoWFactory
+    {
+        private readonly string _nameOrConnectionString;
+
+        public UoWFactory(string nameOrConnectionString = "DefaultConnection")
+        {
+            _nameOrConnectionString = nameOrConnectionString;
+        }
+
+        public IUnitOfWork CreateNew()
+        {
+            var context = new ApplicationDbContext(_nameOrConnectionString);
+
+            var bootstrapper = new ApplicationDbBootstrapper(context);
+            bootstrapper.Initialize();
+
+            return new UnitOfWork(context);
+        }
+    }
+}
